60404e6b160089f05f880c7f0513676ae9c48b60
china-shang <850747813@qq.com>
2017-06-30 12:06:28 +0800

    async1.py ---

--- async1.py
+++ async1.py
@@ -1,30 +1,45 @@
-#!/usr/bin/env python
+#!usr/bin/env python
 # coding=utf-8
 import asyncio
 
+async def base(arg):
+    if arg == 0:
+        print("base:",arg)
+        return True
+    else:
+        print("base:",arg)
+        return False
+
 async def hello():
-    print("this cout")
-    await asyncio.sleep(0.5)
-async def input1():
-    await asyncio.sleep(1)
-    a=input("put \n")
-    return a
-async def server():
-    cout=0
+
     while True:
+        for i in range(10,-1,-1):
+            i=await base(i) 
+            if i == True:
+                print("await base=",i)
+                return True
+
+async def input1():
+    print("input")
+async def server():
+    print("server start")
+    cout=0
+    while cout<3:
+        print("<<\n")
         a=await input1()
         cout=cout+1
+        print(a,"cout=",cout,'\n')
         if a=="exit":
             loop.stop()
             return
         await hello()
-        print(a,"cout=",cout,'\n')
+        print(">>\n")
+    loop.stop()
 loop=asyncio.get_event_loop()
-help(asyncio)
-future=loop.create_future()
-future.add_done_callback(hello)
-#loop.run_until_complete(asyncio.wait([hello(),hello(),hello(),hello()]))
-loop.run_until_complete(server())
+#future=loop.create_future()
+#future.add_done_callback(hello)
+loop.run_until_complete(asyncio.wait([hello(),hello(),hello(),hello(),input1()]))
+#loop.run_until_complete(server())
 loop.run_forever()
 import os
 def sleep():
